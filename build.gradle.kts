0ea9bd5e735971a0a193fd65a81fa4393939d02a
ddPn08 <pyn.g.1125@gmail.com>
2022-07-16 14:41:57 +0900

    1.1-beta.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "run.dn5"
-version = "1.0"
+version = "1.1-beta.1"
 description = "Synchronize chats between servers under Velocity, BungeeCord."
 val artifactName =  "${rootProject.name}-${rootProject.version}.jar"
 
